05a8a1fe5c61de55c07bbf27c1401238e94889c8
Lastorder <lastorder@yjsoft.xyz>
2017-06-10 13:40:10 +0900

    Delete css
    use css of https://userstyles.org/styles/143689/namuwiki-discuss-image-block

--- cleandiscuss.user.js
+++ cleandiscuss.user.js
@@ -6,34 +6,10 @@
 // @run-at        document-start
 // @version       0.1
 // ==/UserScript==
-(function() {var css = [
-	".res-wrapper .res .r-body img.wiki-image {",
-	"    display: none;",
-	"}"
-].join("\n");
-if (typeof GM_addStyle != "undefined") {
-	GM_addStyle(css);
-} else if (typeof PRO_addStyle != "undefined") {
-	PRO_addStyle(css);
-} else if (typeof addStyle != "undefined") {
-	addStyle(css);
-} else {
-	var node = document.createElement("style");
-	node.type = "text/css";
-	node.appendChild(document.createTextNode(css));
-	var heads = document.getElementsByTagName("head");
-	if (heads.length > 0) {
-		heads[0].appendChild(node);
-	} else {
-		// no head yet, stick it whereever
-		document.documentElement.appendChild(node);
-	}
-}
-
+(function() {
 $( document ).ready(function() {
 	$('.r-hidden-body').each(function( index ) {
 		$(this).parent().parent().remove();
 	});
 });
-
 })();
